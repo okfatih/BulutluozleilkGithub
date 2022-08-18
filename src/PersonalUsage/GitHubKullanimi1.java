ca6ef0a7df46d7afc35a02b10f8d57afd0911a70
Fatih <fatih_ok4@hotmail.com>
2022-08-18 12:56:48 +0200

    firstChange

--- src/PersonalUsage/GitHubKullanimi1.java
+++ src/PersonalUsage/GitHubKullanimi1.java
@@ -3,5 +3,6 @@ package PersonalUsage;
 public class GitHubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Updatedgit");
     }
 }
